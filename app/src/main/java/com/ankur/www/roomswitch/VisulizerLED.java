bea7073b85aeaabdac6786e8d6363e026f47842a
Ankur Nigam <ankur.nigam198@live.com>
2017-06-07 18:42:13 +0530

    Basic

--- app/src/main/java/com/ankur/www/roomswitch/VisulizerLED.java
+++ app/src/main/java/com/ankur/www/roomswitch/VisulizerLED.java
@@ -6,11 +6,22 @@ import android.media.audiofx.Visualizer;
  * Created by ankur on 07/06/2017.
  */
 
-public class VisulizerLED {
+public class VisulizerLED implements Visualizer.OnDataCaptureListener {
     Visualizer visualizer = new Visualizer(1);
-    byte[] sample = new byte[8];
+    public byte[] sample = new byte[8];
     void start(){
+        visualizer.setDataCaptureListener(this,Visualizer.getMaxCaptureRate(),false,true);
         visualizer.getFft(sample);
         visualizer.setEnabled(true);
+    }
+
+    @Override
+    public void onWaveFormDataCapture(Visualizer visualizer, byte[] waveform, int samplingRate) {
+
+    }
+
+    @Override
+    public void onFftDataCapture(Visualizer visualizer, byte[] fft, int samplingRate) {
+       byte base = fft[0];
     }
 }
